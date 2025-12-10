af971f4f8bc4669da8e725eb2b231500b1deb633
Mouad Oumous <mouadoumous@gmail.com>
2025-12-11 00:07:56 +0100

    instantiating the driver in the config file

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 # This file will hold the configuration for the application.
 
+from util.utils import get_selenium_driver
+
+debug = True
+
+driver = get_selenium_driver()
+# Remove page load timeout (wait forever until page loads)
+driver.set_page_load_timeout(1000)
+
 BASE_URL = "https://mydramalist.com"
