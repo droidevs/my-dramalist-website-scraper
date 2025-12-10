057bbec1db3f88fa7b05f15e8fe49b15be63df19
Mouad Oumous <mouadoumous@gmail.com>
2025-12-11 00:19:50 +0100

    Define 'tags' and linking table schemas

--- conn.py
+++ conn.py
@@ -40,4 +40,20 @@ CREATE TABLE IF NOT EXISTS dramas (
 );
 """)
 
+conn.execute("""
+CREATE TABLE IF NOT EXISTS tags (
+    tag_id INTEGER PRIMARY KEY,       -- MDL tag id (from "th=" in link)
+    name TEXT UNIQUE
+);
+""")
+conn.execute("""
+CREATE TABLE IF NOT EXISTS drama_tags (
+    drama_id INTEGER,
+    tag_id INTEGER,
+    PRIMARY KEY (drama_id, tag_id),
+    FOREIGN KEY (drama_id) REFERENCES dramas(drama_id),
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+);
+""")
+
 cur = conn.cursor()
