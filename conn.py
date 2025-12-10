42dd35ee1aee4e4900ab48300ad2fa97eeb7f53e
Mouad Oumous <mouadoumous@gmail.com>
2025-12-10 23:55:21 +0100

    added a function to establish the connection to the database file where we will store the data scraped

--- conn.py
+++ conn.py
@@ -1,1 +1,19 @@
 # This file will handle the database connection.
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
