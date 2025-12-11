b6c33d47b11bae4227fcdb11dc0e17a346b678fc
Mouad Oumous <mouadoumous@gmail.com>
2025-12-11 01:35:45 +0100

    implemente basic code to scrap first page of the website

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # This is the main entry point for the application.
+
+from scrapers.drama_scraper import scrape_drama
+from scrapers.link_scraper import get_all_dramas_links
+
+
+if __name__ == '__main__':
+    links = get_all_dramas_links(1)
+    for i,link in enumerate(links):
+        scrape_drama(link)
