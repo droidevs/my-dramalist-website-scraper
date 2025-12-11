909fbd59bcc35db988696808ff36b8861dace601
Mouad Oumous <mouadoumous@gmail.com>
2025-12-11 07:27:28 +0100

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scrapers.link_scraper import get_all_dramas_links
 
 
 if __name__ == '__main__':
-    for page in range(5,20):
+    for page in range(0,1):
         links = get_all_dramas_links(page)
         for i,link in enumerate(links):
             scrape_drama(page,i,link)
