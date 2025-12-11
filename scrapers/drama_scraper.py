cb3557c5351de639563e88b69996f33c8a1070ba
Mouad Oumous <mouadoumous@gmail.com>
2025-12-11 01:29:52 +0100

    Add basic structure for drama scraper function

--- scrapers/drama_scraper.py
+++ scrapers/drama_scraper.py
@@ -1,1 +1,10 @@
 # This file will contain the code to scrape drama details.
+from config import driver
+
+def scrape_drama(url=""):
+    """Scrape a single drama page."""
+    print(f"Started Scraping for drama with link << {url} >> ")
+    driver.get(url)
+    # drama_id from URL
+    drama_id = int(re.search(r"/(\d+)-", url).group(1))
+    print(f"Scraping drama {drama_id}")
